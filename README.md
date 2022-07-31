02cb748224efc3508ac91a0ff0ffb7fc72a037a9
Roy O'Young <roy2220@outlook.com>
2022-07-31 11:58:07 +0800

    Redesign

--- README.md
+++ README.md
@@ -10,78 +10,49 @@ Tiny testing framework (BDD-style)
 
 ```go
 func TestExample(t *testing.T) {
-        // Define the workspace.
-        type Workspace struct {
-                C  *http.Client
-                In struct { // input
-                        URL string
-                }
-                ExpOut, ActOut struct { // expected output & actual output
-                        StatusCode int
-                }
+        type context struct {
+                url                string
+                expectedStatusCode int
         }
 
-        // Create a test case template.
-        // NOTE: Numbers 1, 2, 3 are step numbers, steps will be executed in ascending order of
-        //       step number.
-        tc := testcase.New().
-                Step(1, func(t *testing.T, w *Workspace) {
-                        // Set up the workspace.
-                        w.C = &http.Client{Transport: &http.Transport{}}
+        tc := testcase.New(func(t *testing.T, c *context) {
+                t.Parallel()
 
-                        t.Cleanup(func() {
-                                // Clean up the workspace.
-                                // NOTE: Cleanups will be executed after all steps are executed
-                                //       or panics occur.
-                                w.C.CloseIdleConnections()
-                        })
-                }).
-                Step(2, func(t *testing.T, w *Workspace) {
-                        // Do the test.
-                        resp, err := w.C.Get(w.In.URL)
-                        if !assert.NoError(t, err) {
-                                t.FailNow()
-                        }
-                        w.ActOut.StatusCode = resp.StatusCode
-                        resp.Body.Close()
-                }).
-                Step(3, func(t *testing.T, w *Workspace) {
-                        // Compare the actual output with the expected output.
-                        assert.Equal(t, w.ExpOut, w.ActOut)
-                })
+                client := &http.Client{Transport: &http.Transport{}}
+                defer client.CloseIdleConnections()
 
-        // Make copies of the test case template, insert new steps (1.5) for populating test
-        // data and then run them parallel.
-        // NOTE: Steps in each test case will be executed in order. Test cases will be run with
-        //       isolated workspaces and each step in a test case shares the same workspace.
-        testcase.RunListParallel(t,
-                tc.Copy().
-                        Given("http client").
-                        When("get https://httpbin.org/status/200").
-                        Then("should respond status code 200").
-                        Step(1.5, func(t *testing.T, w *Workspace) { // Step 1.5
-                                // Populate the input & expected output.
-                                w.In.URL = "https://httpbin.org/status/200"
-                                w.ExpOut.StatusCode = 200
-                        }),
-                tc.Copy().
-                        Given("http client").
-                        When("get https://httpbin.org/status/400").
-                        Then("should respond status code 400").
-                        Step(1.5, func(t *testing.T, w *Workspace) { // Step 1.5
-                                // Populate the input & expected output.
-                                w.In.URL = "https://httpbin.org/status/400"
-                                w.ExpOut.StatusCode = 400
-                        }),
-                tc.Copy().
-                        Given("http client").
-                        When("get https://httpbin.org/status/500").
-                        Then("should respond status code 500").
-                        Step(1.5, func(t *testing.T, w *Workspace) { // Step 1.5
-                                // Populate the input & expected output.
-                                w.In.URL = "https://httpbin.org/status/500"
-                                w.ExpOut.StatusCode = 500
-                        }),
-        )
+                testcase.DoCallback(0, t, c)
+
+                resp, err := client.Get(c.url)
+                if !assert.NoError(t, err) {
+                        t.FailNow()
+                }
+                defer resp.Body.Close()
+                assert.Equal(t, c.expectedStatusCode, resp.StatusCode)
+        })
+
+        // get https://httpbin.org/status/200 should response status code 200
+        tc.Copy().
+                SetCallback(0, func(t *testing.T, c *context) {
+                        c.url = "https://httpbin.org/status/200"
+                        c.expectedStatusCode = 200
+                }).
+                Run(t)
+
+        // get https://httpbin.org/status/400 should response status code 400
+        tc.Copy().
+                SetCallback(0, func(t *testing.T, c *context) {
+                        c.url = "https://httpbin.org/status/400"
+                        c.expectedStatusCode = 400
+                }).
+                Run(t)
+
+        // get https://httpbin.org/status/500 should response status code 500
+        tc.Copy().
+                SetCallback(0, func(t *testing.T, c *context) {
+                        c.url = "https://httpbin.org/status/500"
+                        c.expectedStatusCode = 500
+                }).
+                Run(t)
 }
 ```
--- testcase.go
+++ testcase.go
@@ -1,271 +1,137 @@
-// Package testcase provides facilities for table-driven tests.
 package testcase
 
 import (
-	"bytes"
 	"fmt"
 	"path/filepath"
 	"reflect"
 	"runtime"
-	"sort"
+	"sync"
 	"testing"
-	"unsafe"
 )
 
-// TestCase represents a test case.
 type TestCase struct {
-	tc testCase
+	functionValue  reflect.Value
+	functionType   reflect.Type
+	contextPtrType reflect.Type
+	callbackValues map[int]reflect.Value
 }
 
-// Copy copies the test case and returns a clone.
-func (tc *TestCase) Copy() (clone *TestCase) { return tc.tc.Copy().TestCase() }
-
-// Exclude excludes the test case from the list to run.
-func (tc *TestCase) Exclude() (self *TestCase) { return tc.tc.Exclude().TestCase() }
-
-// ExcludeOthers excludes other test cases from the list to run.
-func (tc *TestCase) ExcludeOthers() (self *TestCase) { return tc.tc.ExcludeOthers().TestCase() }
-
-// Given annotates the test case.
-func (tc *TestCase) Given(given string) (self *TestCase) { return tc.tc.Given(given).TestCase() }
-
-// When annotates the test case.
-func (tc *TestCase) When(when string) (self *TestCase) { return tc.tc.When(when).TestCase() }
-
-// Then annotates the test case.
-func (tc *TestCase) Then(then string) (self *TestCase) { return tc.tc.Then(then).TestCase() }
-
-// Step adds a step with the given number to the test case.
-// Steps will be executed in ascending order of step number.
-func (tc *TestCase) Step(stepNo float64, step interface{}) (self *TestCase) {
-	return tc.tc.Step(stepNo, step).TestCase()
-}
-
-// New creates a new test case.
-func New() *TestCase { return new(testCase).Init().TestCase() }
-
-type testCase struct {
-	IsExcluded        bool
-	OthersAreExcluded bool
-
-	locator       string
-	given         string
-	when          string
-	then          string
-	stepType      reflect.Type
-	workspaceType reflect.Type
-	steps         map[float64]interface{}
-}
-
-func (tc *testCase) Init() *testCase {
-	_, fileName, lineNumber, _ := runtime.Caller(2)
-	tc.setLocator(fileName, lineNumber)
-	return tc
-}
-
-func (tc *testCase) Copy() *testCase {
-	clone := testCase{
-		IsExcluded:        tc.IsExcluded,
-		OthersAreExcluded: tc.OthersAreExcluded,
-
-		given:         tc.given,
-		when:          tc.when,
-		then:          tc.then,
-		stepType:      tc.stepType,
-		workspaceType: tc.workspaceType,
+func New(function interface{}) *TestCase {
+	var tc TestCase
+	tc.functionValue = reflect.ValueOf(function)
+	tc.functionType = tc.functionValue.Type()
+	if !validateFunctionType(tc.functionType) {
+		panic(fmt.Sprintf("the type of `function` should be func(*testing.T, *context)"))
 	}
-	_, fileName, lineNumber, _ := runtime.Caller(2)
-	clone.setLocator(fileName, lineNumber)
-	clone.steps = tc.copySteps()
-	return &clone
+	tc.contextPtrType = tc.functionType.In(1)
+	return &tc
 }
 
-func (tc *testCase) setLocator(fileName string, lineNumber int) {
-	shortFileName := filepath.Base(fileName)
-	tc.locator = fmt.Sprintf("%s:%d", shortFileName, lineNumber)
-}
-
-func (tc *testCase) copySteps() map[float64]interface{} {
-	stepsClone := make(map[float64]interface{}, len(tc.steps))
-	for stepNo, step := range tc.steps {
-		stepsClone[stepNo] = step
+func validateFunctionType(functionType reflect.Type) bool {
+	if functionType.Kind() != reflect.Func {
+		return false
 	}
-	return stepsClone
+	if functionType.NumIn() != 2 {
+		return false
+	}
+	if functionType.In(0) != reflect.TypeOf((*testing.T)(nil)) {
+		return false
+	}
+	if functionType.In(1).Kind() != reflect.Ptr {
+		return false
+	}
+	if functionType.NumOut() != 0 {
+		return false
+	}
+	return true
 }
 
-func (tc *testCase) Exclude() *testCase {
-	tc.IsExcluded = true
+func (tc *TestCase) Copy() *TestCase {
+	callbackValues := tc.copyCallbackValues()
+	return &TestCase{
+		functionValue:  tc.functionValue,
+		functionType:   tc.functionType,
+		contextPtrType: tc.contextPtrType,
+		callbackValues: callbackValues,
+	}
+}
+
+func (tc *TestCase) copyCallbackValues() map[int]reflect.Value {
+	if tc.callbackValues == nil {
+		return nil
+	}
+	callbackValues := make(map[int]reflect.Value, len(tc.callbackValues))
+	for callbackID, callbackValue := range tc.callbackValues {
+		callbackValues[callbackID] = callbackValue
+	}
+	return callbackValues
+}
+
+func (tc *TestCase) SetCallback(callbackID int, callback interface{}) *TestCase {
+	callbackValue := reflect.ValueOf(callback)
+	if callbackValue.Type() != tc.functionType {
+		panic(fmt.Sprintf("the type of `callback` should be %s", tc.functionType))
+	}
+	callbackValues := tc.callbackValues
+	if callbackValues == nil {
+		callbackValues = make(map[int]reflect.Value)
+		tc.callbackValues = callbackValues
+	}
+	callbackValues[callbackID] = callbackValue
 	return tc
 }
 
-func (tc *testCase) ExcludeOthers() *testCase {
-	tc.OthersAreExcluded = true
-	return tc
-}
+var (
+	testCasesLock sync.RWMutex
+	testCases     = map[*testing.T]*TestCase{}
+)
 
-func (tc *testCase) Given(given string) *testCase {
-	tc.given = given
-	return tc
-}
-
-func (tc *testCase) When(when string) *testCase {
-	tc.when = when
-	return tc
-}
-
-func (tc *testCase) Then(then string) *testCase {
-	tc.then = then
-	return tc
-}
-
-func (tc *testCase) Step(stepNo float64, step interface{}) *testCase {
-	tc.validateStepType(step, stepNo)
-	tc.addStep(stepNo, step)
-	return tc
-}
-
-func (tc *testCase) validateStepType(step interface{}, stepNo float64) {
-	stepType := reflect.TypeOf(step)
-	if tc.stepType != nil {
-		if stepType != tc.stepType {
-			panic(fmt.Sprintf("step type mismatch; stepNo=%v stepType=%q expectedStepType=%q",
-				stepNo, stepType, tc.stepType))
+func (tc *TestCase) Run(t *testing.T) {
+	name := makeName()
+	t.Run(name, func(t *testing.T) {
+		testCasesLock.Lock()
+		testCases[t] = tc
+		testCasesLock.Unlock()
+		defer func() {
+			testCasesLock.Lock()
+			delete(testCases, t)
+			testCasesLock.Unlock()
+		}()
+		argsValues := [...]reflect.Value{
+			reflect.ValueOf(t),
+			reflect.New(tc.contextPtrType.Elem()),
 		}
-		return
-	}
-	if stepType.Kind() != reflect.Func {
-		panic(fmt.Sprintf("step should be function; stepNo=%v stepType=%q", stepNo, stepType))
-	}
-	if stepType.NumIn() != 2 {
-		panic(fmt.Sprintf("step should have exactly two parameters; stepNo=%v stepType=%q",
-			stepNo, stepType))
-	}
-	if stepType.In(0) != reflect.TypeOf((*testing.T)(nil)) {
-		panic(fmt.Sprintf("step parameter #1 should be pointer to testing.T; stepNo=%v stepType=%q",
-			stepNo, stepType))
-	}
-	workspaceType, ok := func() (reflect.Type, bool) {
-		workspaceTypePtr := stepType.In(1)
-		if workspaceTypePtr.Kind() != reflect.Ptr {
-			return nil, false
-		}
-		workspaceType := workspaceTypePtr.Elem()
-		if workspaceType.Kind() != reflect.Struct {
-			return nil, false
-		}
-		return workspaceType, true
-	}()
-	if !ok {
-		panic(fmt.Sprintf("step parameter #2 should be pointer to structure; stepNo=%v stepType=%q",
-			stepNo, stepType))
-	}
-	if stepType.NumOut() != 0 {
-		panic(fmt.Sprintf("step should not return any results; stepNo=%v stepType=%q", stepNo, stepType))
-	}
-	tc.stepType = stepType
-	tc.workspaceType = workspaceType
-}
-
-func (tc *testCase) addStep(stepNo float64, step interface{}) {
-	steps := tc.steps
-	if steps == nil {
-		steps = make(map[float64]interface{}, 1)
-		tc.steps = steps
-	} else {
-		if _, ok := steps[stepNo]; ok {
-			panic(fmt.Sprintf("duplicate step number; stepNo=%v", stepNo))
-		}
-	}
-	steps[stepNo] = step
-}
-
-func (tc *testCase) Run(t *testing.T, parallel bool) {
-	if tc.steps == nil {
-		panic("no step")
-	}
-	t.Run(tc.locator, func(t *testing.T) {
-		if parallel {
-			t.Parallel()
-		}
-		tc.logGWT(t)
-		tc.executeSteps(t)
+		tc.functionValue.Call(argsValues[:])
 	})
 }
 
-func (tc *testCase) logGWT(t *testing.T) {
-	var buffer bytes.Buffer
-	if tc.given != "" {
-		buffer.WriteString("\nGIVEN " + tc.given)
-	}
-	if tc.when != "" {
-		buffer.WriteString("\nWHEN " + tc.when)
-	}
-	if tc.then != "" {
-		buffer.WriteString("\nTHEN " + tc.then)
-	}
-	if buffer.Len() == 0 {
-		return
-	}
-	t.Log(buffer.String())
+func makeName() string {
+	_, fileName, lineNumber, _ := runtime.Caller(2)
+	shortFileName := filepath.Base(fileName)
+	return fmt.Sprintf("<%s:%d>", shortFileName, lineNumber)
 }
 
-func (tc *testCase) executeSteps(t *testing.T) {
-	steps := tc.sortSteps()
-	tValuePtr := reflect.ValueOf(t)
-	workspaceValuePtr := reflect.New(tc.workspaceType)
-	args := []reflect.Value{tValuePtr, workspaceValuePtr}
-	for _, step := range steps {
-		reflect.ValueOf(step).Call(args)
+func DoCallback(callbackID int, t *testing.T, context interface{}) {
+	testCasesLock.RLock()
+	testCase, ok := testCases[t]
+	testCasesLock.RUnlock()
+	if !ok {
+		panic("should only be called from test case functions")
 	}
+	testCase.doCallback(callbackID, t, context)
 }
 
-func (tc *testCase) sortSteps() []interface{} {
-	stepNos := make([]float64, len(tc.steps))
-	i := 0
-	for stepNo := range tc.steps {
-		stepNos[i] = stepNo
-		i++
+func (tc *TestCase) doCallback(callbackID int, t *testing.T, context interface{}) {
+	callbackValue, ok := tc.callbackValues[callbackID]
+	if !ok {
+		panic(fmt.Sprintf("can't find callback by id - %d", callbackID))
 	}
-	sort.Float64s(stepNos)
-	steps := make([]interface{}, len(tc.steps))
-	i = 0
-	for _, stepNo := range stepNos {
-		steps[i] = tc.steps[stepNo]
-		i++
+	argsValues := [...]reflect.Value{
+		reflect.ValueOf(t),
+		reflect.ValueOf(context),
 	}
-	return steps
-}
-
-func (tc *testCase) TestCase() *TestCase { return (*TestCase)(unsafe.Pointer(tc)) }
-
-// RunList runs the given list of test cases, steps in each test case will be
-// executed in order.
-// Test cases will be run with isolated workspaces and each step in a test case
-// shares the same workspace.
-func RunList(t *testing.T, list ...*TestCase) {
-	doRunList(t, list, false)
-}
-
-// RunListParallel runs the given list of test cases parallel, steps in each test case
-// will be executed in order.
-// Test cases will be run with isolated workspaces and each step in a test case
-// shares the same workspace.
-func RunListParallel(t *testing.T, list ...*TestCase) {
-	doRunList(t, list, true)
-}
-
-func doRunList(t *testing.T, list []*TestCase, parallel bool) {
-	for _, tc := range list {
-		tc := &tc.tc
-		if tc.OthersAreExcluded {
-			tc.Run(t, false)
-			return
-		}
+	if argsValues[1].Type() != tc.contextPtrType {
+		panic(fmt.Sprintf("the type of `context` should be %s", tc.contextPtrType))
 	}
-	for _, tc := range list {
-		tc := &tc.tc
-		if !tc.IsExcluded {
-			tc.Run(t, parallel)
-		}
-	}
+	callbackValue.Call(argsValues[:])
 }
